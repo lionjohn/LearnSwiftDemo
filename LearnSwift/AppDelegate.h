53d99cc324d14200ed4812fbe8fc40e018eabdfb
John <john.mao@sap.com>
2017-06-16 14:52:09 +0800

    Initial Commit

---
+++ LearnSwift/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LearnSwift
+//
+//  Created by John on 6/16/17.
+//  Copyright © 2017 LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LearnSwift/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LearnSwift
+//
+//  Created by John on 6/16/17.
+//  Copyright © 2017 LJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
